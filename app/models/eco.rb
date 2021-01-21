9c4848316cd6bdd64551ef7bcdee66c7953dc551
Ubuntu <ubuntu@ip-172-31-51-4.ec2.internal>
2021-01-21 07:44:50 +0000

    validation

--- app/models/eco.rb
+++ app/models/eco.rb
@@ -1,2 +1,4 @@
 class Eco < ApplicationRecord
+  validates :action, presence: true, length: { maximum: 255 }
+  validates :co2, numericality: { only_integer: true, greater_than: 0, less_than: 5000 }
 end
