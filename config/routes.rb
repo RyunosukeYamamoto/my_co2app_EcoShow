edf8a0690cab9ac0e58f5cff9fc30c679fd453b8
Ubuntu <ubuntu@ip-172-31-51-4.ec2.internal>
2021-01-21 18:06:23 +0000

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'ecos#index'
   
+  get 'signup', to: 'users#new'
   resources :ecos, except: [:show]
+  resources :users, only: [:show, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_144405) do
+ActiveRecord::Schema.define(version: 2021_01_21_165915) do
 
   create_table "ecos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "action"
     t.integer "co2"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
