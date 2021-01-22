ae61b7123090afe20f887aef3462c8463c445124
Ubuntu <ubuntu@ip-172-31-51-4.ec2.internal>
2021-01-22 15:07:45 +0000

    my_eco_action to my_car_distance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,25 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+  
+  def counts(user)
+    @count_ecos = user.ecos.count
+  end
+  
+  def car_distance(x)
+    if x >= 400
+      @prace = '大阪'
+    elsif x >= 300
+      @prace = '琵琶湖'
+    elsif x >= 200
+      @prace = '愛知'
+    elsif x >= 100
+      @prace = '富士山'
+    elsif x >= 50
+      @prace = '相模原'
+    else
+      @prace = '府中'
+    end
+    return @prace
+  end
 end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,9 +3,15 @@ class ToppagesController < ApplicationController
     @ecos = Eco.order(id: :desc).page(params[:page]).per(15)
     @total_co2 = Eco.all.sum(:co2)
     @kiro = @total_co2 / 230
+    @total_car_distance = car_distance(@kiro)
+    
     if logged_in?
       @my_ecos = current_user.ecos.order(id: :desc).page(params[:page])
+      @my_total_co2 = @my_ecos.all.sum(:co2)
+      @my_kiro = @my_total_co2 / 230
       @my_eco = current_user.ecos.build  # form_with用
+      counts(current_user)
+      @my_total_car_distance = car_distance(@my_kiro) # my到達地点の格納
     end
   end
 end
